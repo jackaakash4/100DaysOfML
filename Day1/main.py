ea767d3cbe94c05be09853c58a9d71f03cfea9c3
Jack Aakash <jackaakash4@gmail.com>
2026-06-22 11:34:12 +0545

    Day 01: Core essential of python i.e. list, tuple, dict, function

--- Day1/main.py
+++ Day1/main.py
@@ -17,3 +17,36 @@ image_shape = (224, 224, 3)
 #booleans control training flags
 use_gpu = True
 early_stop = False
+
+
+#control flow
+#the heart of every ML training run
+
+for epoch in range(100):
+    loss = train_one_epouch(model, data)
+
+    if loss < best_loss:
+        best_loss = loss
+        save_checkpoint(model)
+
+    elif patience_counter > 5:
+        break
+
+
+#Function: reusuable pipeline steps
+
+def normalize(value, min_val, max_val):
+    #scaling values to [0, 1] - standard ML preprocessing
+    return [(v - min_val)/ (max_val - min_val) for v in values]
+
+def train_epoch(model, loader, optimizer):
+    #one full pass over the training data
+    total_loss = 0
+    for batch in loader:
+        optimizer.zero_grad()
+        loss = model.forward(batch)
+        loss.backward()
+        optimizer.step()
+        total_loss + = loss.item()
+    return total_loss / len(loader)
+
