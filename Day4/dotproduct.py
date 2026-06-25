b7d2a760d1f7f8431084172263530ef36a80ba54
Jack Aakash <jackaakash4@gmail.com>
2026-06-25 12:10:38 +0545

    Vector and matrix multiplication

--- Day4/dotproduct.py
+++ Day4/dotproduct.py
@@ -7,3 +7,12 @@ v2 = np.array([3, 4])
 #compute
 print(f"Dot product of {v1} and {v2} is {np.dot(v1, v2)}")
 
+
+#vector matrix multiplication
+matrix = np.array([
+    [3, 4],
+    [5, 6]
+    ])
+
+result = np.dot(v1, matrix)
+print(f"Vector matrix multiplication of{v1} and {matrix} is {result}")
