0222410ad8e713552799a2582f661cf26e5ded0c
Jack Aakash <jackaakash4@gmail.com>
2026-06-30 12:30:19 +0545

    Day09: Mean, variance and standard deviation

---
+++ Day9/feature_scale.py
@@ -0,0 +1,16 @@
+#Day9
+#Feature scales: Variance and standard deviation
+
+import numpy as np
+
+scores = np.array([40, 42, 45, 43, 44])
+#mean   
+print("Mean: ",np.mean(scores))
+
+#variance
+print("Variance: ", np.var(scores))
+
+#standard deviation
+print("Standard deviation: ", np.std(scores))
+
+#
