37af8e98d1c96177757e6f07b358ea7ec75f2c42
Jack Aakash <jackaakash4@gmail.com>
2026-07-01 20:40:54 +0545

    Day09: Normal and Poission distribution visulization

--- Day9/distribution.py
+++ Day9/distribution.py
@@ -1,5 +1,7 @@
 #Distribution recognization
 #normal distribution
+#mean = 0
+#sd = 1
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -14,3 +16,14 @@ plt.hist(normal, bins = 40)
 plt.title("Normal distribution ")
 plt.show()
 
+#Poisson Distribution
+#models counts
+
+poisson = np.random.poisson(
+        lam = 5,
+        size = 1000
+        )
+
+plt.hist(poisson, bins = 20)
+plt.title("Poisson Distribution")
+plt.show()
